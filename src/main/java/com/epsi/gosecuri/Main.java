edc703d797fbb0ff102307c6e949021c94bfd366
Cyriel89 <anthony.carrer@hotmail.fr>
2022-03-06 17:08:16 +0100

    repair

--- src/main/java/com/epsi/gosecuri/Main.java
+++ src/main/java/com/epsi/gosecuri/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Files index = new Files();
-        index.CreateHtpasswdAdmin();
+        //index.CreateHtpasswdAdmin();
         index.ReadStaffListFile();
         index.ReadToolsFile();
         index.ReadAgentFile();
