d33d01c0b687592cfc6ece0d6afd9c4c877f69fc
Cyriel89 <anthony.carrer@hotmail.fr>
2022-03-06 18:17:46 +0100

    test path

--- src/main/java/com/epsi/gosecuri/Files.java
+++ src/main/java/com/epsi/gosecuri/Files.java
@@ -10,7 +10,7 @@ public class Files {
     private final Map<String, String> toolsList = new HashMap<>();
     private final List<Agent> agents = new ArrayList<>();
 
-    private final String path = "/mnt/www/html/src/main/java/com/epsi/gosecuri/assets/";
+    private final String path = "src\\main\\java\\com\\epsi\\gosecuri\\assets\\";
     private final String Header = """
             <!doctype html>
             <html lang="fr">
@@ -43,6 +43,7 @@ public class Files {
 
     public void ReadStaffListFile(){
         try {
+            System.out.println(new File(".").getAbsolutePath());
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"staff.txt"));
             String str;
             while ((str = bufferedReader.readLine()) != null){
@@ -77,7 +78,7 @@ public class Files {
         for (String a : staffList){
             try{
                 toolsListAgent = new ArrayList<>();
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"agents/" + a + ".txt"));
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"agents\\" + a + ".txt"));
                 int i = 1;
                 String str;
                 while((str = bufferedReader.readLine()) != null){
@@ -119,8 +120,8 @@ public class Files {
     public void CreateIndex(){
         String agentsList = getAgentListNames();
         try{
-            new FileOutputStream(path+"web/index.html", false).close();
-            FileWriter writeIndex = new FileWriter(path+"web/index.html");
+            new FileOutputStream(path+"web\\index.html", false).close();
+            FileWriter writeIndex = new FileWriter(path+"web\\index.html");
             writeIndex.write(Header);
             writeIndex.write(agentsList);
             writeIndex.write(Footer);
@@ -134,8 +135,8 @@ public class Files {
         //String agentTools = getAgentData();
         for (Agent a : agents) {
             try{
-                new FileOutputStream(path+"web/"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html", false).close();
-                FileWriter writeAgent = new FileWriter(path+"web/"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html");
+                new FileOutputStream(path+"web\\"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html", false).close();
+                FileWriter writeAgent = new FileWriter(path+"web\\"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html");
                 writeAgent.write(Header);
                 writeAgent.write("<div><h1>"+ a.getSurname()+ " " + a.getName() +"</h1></div>" +
                                  "<div><p>Mission : "+ a.getAssignment() + "</p></div>" +
