094a730242f59dbc1fb6b5d85c2517a29dce377b
Cyriel89 <anthony.carrer@hotmail.fr>
2022-03-07 19:52:09 +0100

    add comments

--- src/main/java/com/epsi/gosecuri/Files.java
+++ src/main/java/com/epsi/gosecuri/Files.java
@@ -10,7 +10,9 @@ public class Files {
     private final Map<String, String> toolsList = new HashMap<>();
     private final List<Agent> agents = new ArrayList<>();
 
+    // Relative Path
     private final String path = "Pipeline-MSPR/src/main/java/com/epsi/gosecuri/assets/";
+    // HTML template
     private final String Header = """
             <!doctype html>
             <html lang="fr">
@@ -35,12 +37,14 @@ public class Files {
                     <div class="row content-container">
                         <div class="col">
             """;
-
     private final String Footer = """
                     </div>
                 </div>
             </main></body>""".indent(4);
 
+    /**
+     * Read the file that contains the list of agents
+     */
     public void ReadStaffListFile(){
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"staff.txt"));
@@ -54,11 +58,15 @@ public class Files {
         }
     }
 
+    /**
+     * Read the file that contains the list of tools
+     */
     public void ReadToolsFile(){
         try{
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"liste.txt"));
             String str;
             while ((str = bufferedReader.readLine()) != null){
+                //Split the line at the tabulation (\t)
                 this.toolsList.put(str.split("\t")[0], str.split("\t")[1]);
             }
             bufferedReader.close();
@@ -67,6 +75,9 @@ public class Files {
         }
     }
 
+    /**
+     * Read the file that contains the infos of one agent
+     */
     public void ReadAgentFile(){
         String name = null;
         String surname = null;
@@ -80,9 +91,9 @@ public class Files {
                 BufferedReader bufferedReader = new BufferedReader(new FileReader(path+"agents/" + a + ".txt"));
                 int i = 1;
                 String str;
+                // For each line in the file I collect the data and put them in variables
                 while((str = bufferedReader.readLine()) != null){
                     if (!str.isEmpty()){
-
                         if (i == 1){
                             name = str;
                         }else if (i == 2){
@@ -98,15 +109,21 @@ public class Files {
                     }
                 }
                 bufferedReader.close();
+                // Instantiate an agent
                 Agent agent = new Agent(name, surname, pwd, assignment, toolsListAgent);
                 agents.add(agent);
-                //CreateHtpasswd(agent);
+                // Create the Password for the .htpassword
+                CreateHtpasswd(agent);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * loop over all agents to create an HTML list of link to the content of an agent
+     * @return result -> the string that contains the HTML list of links
+     */
     public String getAgentListNames(){
         String result = "<h1>Liste des agents</h1>";
         for (Agent a : agents) {
@@ -115,11 +132,17 @@ public class Files {
         return result;
     }
 
+    /**
+     * Create the HTML file for the index and write the agents list on it
+     */
     public void CreateIndex(){
+        // Get the list of links to the page of each agent
         String agentsList = getAgentListNames();
         try{
+            // Create the HTML file
             new FileOutputStream(path+"web/index.html", false).close();
             FileWriter writeIndex = new FileWriter(path+"web/index.html");
+            // Write the HTML template and the list of agents
             writeIndex.write(Header);
             writeIndex.write(agentsList);
             writeIndex.write(Footer);
@@ -129,18 +152,25 @@ public class Files {
         }
     }
 
+    /**
+     * Create an HTML file for each agent and writes his content
+     */
     public void CreateAgentPage(){
-        //String agentTools = getAgentData();
+        // loop over each agent
         for (Agent a : agents) {
             try{
-                System.out.println(a.getPhoto());
+                // Create the HTML file
                 new FileOutputStream(path+"web/"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html", false).close();
                 FileWriter writeAgent = new FileWriter(path+"web/"+ (a.getSurname().charAt(0)+a.getName()).toLowerCase() +".html");
+                // Write the HTML template
                 writeAgent.write(Header);
+                // Write the infos of an agent
                 writeAgent.write("<div><h1>"+ a.getSurname()+ " " + a.getName() +"</h1></div>" +
                                  "<div><p>Mission : "+ a.getAssignment() + "</p></div>" +
                                  "<div><img src=\"../images/"+ a.getPhoto().toLowerCase() + "\" alt=\"Carte d'identité de "+ a.getSurname()+ " " + a.getName() +"\"></div></div>" +
                                  "<div class=\"checklist_container\"><div class=\"col checklist\">");
+                // Get the tools for each agents
+                // Check in the HashMap if the tools equals the key and get the value
                 for (String tools : this.toolsList.values()){
                         if (a.getToolsList().contains(tools)){
                             writeAgent.write( "<div class=\"form-check\"><input class=\"checkbox\" type=\"checkbox\" value=\""+ tools + "\" id=\"id"+tools.substring(0, 4)+"\" disabled checked readonly>\n" +
@@ -163,8 +193,12 @@ public class Files {
             }
         }
     }
-    /*public void CreateHtpasswdAdmin(){
-        String str = "admin:{SHA}"+ Base64.getEncoder().encodeToString(DigestUtils.sha1("admin1234"));
+
+    /**
+     * Create the .htpasswd with the admin logs
+     */
+    public void CreateHtpasswdAdmin(){
+        String str = "admin:{SHA}"+ Base64.getEncoder().encodeToString(DigestUtils.sha1("admin"));
         try {
             new FileOutputStream(".htpasswd", false).close();
             BufferedWriter writeHtpasswd = new BufferedWriter(new FileWriter(".htpasswd",true));
@@ -176,6 +210,10 @@ public class Files {
         }
     }
 
+    /**
+     * Create the password for each agent
+     * @param a -> an agent
+     */
     public void CreateHtpasswd(Agent a){
         String str = (a.getSurname().charAt(0)+a.getName()).toLowerCase()+":{SHA}"+ Base64.getEncoder().encodeToString(DigestUtils.sha1(a.getPassword()));
         try {
@@ -186,5 +224,5 @@ public class Files {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }*/
+    }
 }
